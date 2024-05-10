b548374f6e37946b7a5b685b178e752a51d4ae40
Dayyan Shaikh <dayyanshaikh16@gmail.com>
2024-05-11 00:38:11 +0530

    first commit

---
+++ src/components/Body.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="space-y-4">
+      <div className="flex items-center justify-center">
+        <img src="./assets/Blue-Shape.svg" className=" -rotate-45 h-64 md:h-72" />
+        <img
+          src="./assets/Pink-Shape.svg"
+          className="absolute -rotate-[30deg] h-64 md:h-72"
+        />
+        <img
+          src="./assets/Purple-Shape.svg"
+          className="absolute -rotate-[15deg] h-64 md:h-72"
+        />
+        <img src="./assets/Hero-Model.png" className="absolute h-64 md:h-72" />
+      </div>
+      <div>
+        <h1 className="text-5xl font-bold leading-tight">Host your website in less than 5 minutes</h1>
+        <p className="text-gray-400">
+          With Hoster , get your website up and running in no less than 5
+          minutes with the most competitive pricing packages available online
+        </p>
+        <form action="" className="flex flex-col gap-4 md:flex-row">
+          <input className="rounded-md px-4 py-3 placeholder:text-grey-400" type="email" placeholder="Enter email address" />
+          <button className="rounded-md px-4 py-3 bg-blue-400 hover:bg-blue-600 text-white">Join Waitlist</button>
+        </form>
+        <div className="flex gap-2">
+          <img src="./assets/Checkmark.svg" />
+          <p>No spam ,ever . Unsubscribe anytime</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { FaBars } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <div className="flex justify-between">
+      <div className="flex items-center justify-center gap-2">
+        <img src="./assets/Logo.svg" />
+        <button className="bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white px-2.5 py-1.5 rounded-2xl">Hoster is hiring</button>
+      </div>
+      <div>
+        <FaBars />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
